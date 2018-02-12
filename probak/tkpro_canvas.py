036b25a408b36ce157b8598d7bf1faf86a02bb51
HaaZee <haazee4droid@gmail.com>
2018-02-13 00:32:27 +0100

    Mozgó, random grafikon próba

--- probak/tkpro_canvas.py
+++ probak/tkpro_canvas.py
@@ -10,14 +10,17 @@ def start():
 	
 	canvas=tk.Canvas(top, width=1000, height=800)
 	canvas.pack()
+	top.update()
 	print("Started")
 	time.sleep(2)
-	canvas.create_line(0,400,1,700)
+	canvas.create_line(0,400,10,700,width=4,fill="#0000ff")
+	canvas.update()
 	time.sleep(2)
-	canvas.create_line(1,700,2,200)
+	canvas.create_line(10,700,20,200,width=4,fill="#00ff00")
+	canvas.update()
 	time.sleep(2)
-	canvas.create_line(2,200,5,500)
-	canvas.pack()
+	canvas.create_line(20,200,50,500,width=4,fill="#ff0000")
+	canvas.update()
 	top.mainloop()
 	
 	
