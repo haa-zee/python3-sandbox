0c3fb647fd407ffb1314af181fb73e80a1cc872a
HaaZee <haazee4droid@gmail.com>
2018-02-21 21:30:13 +0100

    mproc_proba_v2.py az eredeti mproc_proba.py átalakított verziója, ahol a ping válaszait egy queue-n keresztül, egy külön szálhoz küldöm "feldolgozásra".

--- probak/mproc_proba_v2.py
+++ probak/mproc_proba_v2.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import shlex, subprocess
+import shlex, subprocess, threading, queue
+E_DATA = '1'
+E_COMMAND = '0'
 
-print("Hello")
 
-cmd=shlex.split("/bin/ping 8.8.8.8 -i2 -c10")
-print(cmd)
+def producer_thread(queue_object, ip_addr):
+    cmd = shlex.split("/bin/ping {} -i2 -c10".format(ip_addr))
 
-with subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT) as proc:
-    for l in proc.stdout:
-        print("-->",type(l)," ",l.decode().rstrip("\n"))
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
+        for l in proc.stdout:
+            queue_object.put(E_DATA+l.decode().rstrip("\n"))
+        queue_object.put(E_COMMAND)
 
-print('Itt a vége?')
 
+def consumer_thread(queue_object):
+    message = queue_object.get()
+    while message[0] != E_COMMAND:
+        print("--> {}".format(message[1:]))
+        message = queue_object.get()
+    print("--- VÉGE ---")
+
+
+def main():
+    q = queue.Queue()
+    t1 = threading.Thread(target=producer_thread, args=(q, "8.8.8.8", ))
+    t2 = threading.Thread(target=consumer_thread, args=(q, ))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+
+if __name__ == "__main__":
+    print("Hello")
+    main()
+    print('Itt a vége?')
