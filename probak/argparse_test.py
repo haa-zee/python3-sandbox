7f4d76f604c983d9fd3a0f4ecc6d7bd5b183b5aa
HaaZee <haazee4droid@gmail.com>
2019-01-01 02:30:48 +0100

    argparse test/demo

--- probak/argparse_test.py
+++ probak/argparse_test.py
@@ -2,13 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import argparse
 
 
-def start(args):
-    print("Eredeti sys.argv:",args)
-    parser = argparse.ArgumentParser(description="Teszt program")
-    parser.add_argument()
+class DirType:
+    def __init__(self):
+        pass
+
+    def __call__(self, dirname):
+        if not os.path.exists(dirname) or not os.path.isdir(dirname):
+            raise argparse.ArgumentError(dirname)
+        return dirname
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    cmd_subparsers = parser.add_subparsers(
+        title="Commands", description="Log file management commands",
+        dest="CMD"
+    )
+    cmd_list = cmd_subparsers.add_parser("list", help="List files")
+    cmd_list.add_argument("directory", type=DirType())
+
+    cmd_view = cmd_subparsers.add_parser("view", help="View the file")
+    cmd_view.add_argument("file", type=argparse.FileType('r'))
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    start(sys.argv)
+    print(parse_arguments())
